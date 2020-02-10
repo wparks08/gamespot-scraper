ee37b641b03bdfb384fc735422337726809ba4fa
Will Parks <wparks08@gmail.com>
2020-02-10 14:50:55 -0800

    update article to make links unique and store image urls

--- models/article.js
+++ models/article.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 const articleSchema = new Schema({
     title: String,
     description: String,
-    link: String,
+    link: {
+        type: String,
+        unique: true
+    },
     posted: Date,
     comments: [
         {
@@ -15,7 +18,8 @@ const articleSchema = new Schema({
     saved: {
         type: Boolean,
         default: false
-    }
+    },
+    image: String
 });
 
 const article = mongoose.model("article", articleSchema);
